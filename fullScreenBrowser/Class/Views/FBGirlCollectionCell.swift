1532eadefae236973e2ba01d21ca4224a353b984
iHTCboy <ihetiancong@qq.com>
2016-11-05 22:58:03 +0800

    add FBGirlCollectionCell

---
+++ fullScreenBrowser/Class/Views/FBGirlCollectionCell.swift
@@ -0,0 +1,22 @@
+//
+//  FBGirlCollectionCell.swift
+//  FullScreenBrowser
+//
+//  Created by HTC on 2016/11/5.
+//  Copyright © 2016年 iHTCboy. All rights reserved.
+//
+
+import UIKit
+
+class FBGirlCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        
+    }
+}
